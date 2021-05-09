d80049470ed53925103fac95b6df5aa10bcc9d21
guizeras <guizeras@gmail.com>
2021-05-09 14:17:31 +0000

    X
    Git Final project

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+   var principal = document.getElementById("principal").value;
+        if(principal == ""){
+            alert("Insert a number");
+            onfocus(principal);
+            return false;
+        }
+   var rate      = document.getElementById("rate").value;
+   var years     = document.getElementById("years").value;
+   var interest  = principal * years * rate / 100;
+   var year      = new Date().getFullYear()+parseInt(years);
+   var amount    = principal - interest;
+   document.getElementById("result").innerHTML="\<br\>If you deposit "
+   +principal+",\<br\>at a Rate of "+rate+"%\<br\>You receive an amount of "+amount+",\<br\>In the year "+year+"\<br\>";
+}
+
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+
 }
         
